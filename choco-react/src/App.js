8a80f01ef9a206a88fa3a521891612a334ffb06d
Michael Romay <amerror@gmail.com>
2020-02-10 20:46:20 -0800

    cleanup

--- choco-react/src/App.js
+++ choco-react/src/App.js
@@ -6,27 +6,7 @@ import Heading from "./Heading.js";
 import ImageText from "./Image-text.js";
 import "./App.css";
 
-const myLogo = "http://www.placekitten.com/100/100";
-
-
 export default class App extends Component {
-  state = {
-    username: "Suzie",
-    usernameInInput: "",
-    counter: 0
-  };
-
-  handleInputChange = e => this.setState({ usernameInInput: e.target.value });
-
-  handleButtonClick = () =>
-    this.setState({
-      username: this.state.usernameInInput
-    });
-
-  handleIncrementClick = e =>
-    this.setState({
-      counter: this.state.counter + 1
-    });
 
   render() {
     return (
